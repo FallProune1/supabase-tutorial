f7266613b53fcedf1e0ef58242900743fa12c251
FallProune1 <jaramillo.alvarado.jetroeliseo@gmail.com>
2024-01-21 01:37:38 -0600

    Primera parte del Landing completada
    Aun falta pulir un poquitito pero ya esta quedando muy bonito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,68 @@
 import Image from "next/image";
+import {
+  IoLogoGithub,
+  IoLogoYoutube,
+  IoLogoTwitter,
+  IoLogoInstagram,
+} from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen min-w-full flex-col items-center justify-between p-24 bg-gradient-to-b from-slate-950 to-neutral-950">
+      <div className="flex flex-col w-5/6">
+        <div className=" mr-auto w-96 flex p-2 text-blue-100 border border-blue-600 rounded-full text-sm">
+          <p className=" text-lg w-25 p-3 bg-blue-600 rounded-full bg-opacity-30 text-blue-200 font-medium">
+            Hola 🖖&nbsp;
+          </p>
+          <div className=" w-60 ml-4 text-base">
+            Conoce la trayectoria profecional de{" "}
+            <strong className="text-blue-500 font-mono">FallProune</strong>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h1 className=" mt-32 text-7xl font-medium text-blue-100 leading-normal">
+          Bienvenido al portafolio de&nbsp;
+          <strong className="glow font-mono font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-rose-600 to-rose-500 ">
+            FallProune
+          </strong>
+        </h1>
+        <div className="flex w-full flex-col">
+          <h3 className=" font-medium text-rose-200 text-xl bg-rose-600 bg-opacity-30 p-4 rounded-full w-40">
+            Sigueme 🚀
+          </h3>
+          <div className="flex text-blue-100 justify-between w-60 text-3xl mt-5 h-10">
+            <a href="https://github.com/FallProune1" target="_blank">
+              <div className="hover:text-white transition-all hover:text-4xl">
+                {" "}
+                <IoLogoGithub></IoLogoGithub>
+              </div>
+            </a>
+            <a href="https://www.instagram.com/fallproune_e/" target="_blank">
+              <div className=" hover:text-pink-500 transition-all hover:text-4xl">
+                {" "}
+                <IoLogoInstagram></IoLogoInstagram>
+              </div>
+            </a>
+            <a href="https://www.youtube.com/@fallproune" target="_blank">
+              <div className="hover:text-red-500 transition-all hover:text-4xl">
+                {" "}
+                <IoLogoYoutube></IoLogoYoutube>
+              </div>
+            </a>
+            <a href="https://twitter.com/fallproune" target="_blank">
+              <div className="hover:text-sky-100 transition-all hover:text-4xl">
+                {" "}
+                <FaXTwitter></FaXTwitter>
+              </div>
+            </a>
+          </div>
+        </div>
+        <p className="text-2xl text-purple-200 mt-10 w-3/4">
+          Unete para descubrir más sobre su{" "}
+          <strong className="text-purple-400 font-mono ">
+            asombroso mundo 🔥🌙.
+          </strong>
+        </p>
       </div>
     </main>
   );
